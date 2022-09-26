430f73d5c723b08fa58e177dd353c9347e4c11e7
Reuben Khadka <noro4921@gmail.com>
2022-09-26 22:05:57 +1000

    Added a sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar } from "./index";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack
+      sx={{
+        flexDirection: { sx: "column", md: "row" },
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright @Reuben
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
